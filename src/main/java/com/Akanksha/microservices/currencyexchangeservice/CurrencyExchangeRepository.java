4b85cf7e52413e84c1cb095bbf0a287ca12b0171
akankshamate <85989430+akankshamate@users.noreply.github.com>
2021-09-20 14:03:02 +0530

    Delete src directory

--- src/main/java/com/Akanksha/microservices/currencyexchangeservice/CurrencyExchangeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.Akanksha.microservices.currencyexchangeservice;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CurrencyExchangeRepository extends JpaRepository<CurrencyExchange,Long> {
-	CurrencyExchange findByFromAndTo(String from,String to);
-}
